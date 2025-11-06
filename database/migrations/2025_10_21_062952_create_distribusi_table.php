7ddd345b15236a0dcefd3a1eec0e04305ff27615
bokerface <waskitodamar17@gmail.com>
2025-11-06 14:55:09 +0700

    nambahi enum karo user_id kolom neng tabel distribusi

--- database/migrations/2025_10_21_062952_create_distribusi_table.php
+++ database/migrations/2025_10_21_062952_create_distribusi_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->foreignId('jadwal_produksi_id')->constrained('jadwal_produksi')->cascadeOnDelete();
             $table->foreignId('sekolah_id')->constrained('sekolah')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->unsignedInteger('jumlah_porsi_besar')->default(0);
             $table->unsignedInteger('jumlah_porsi_kecil')->default(0);
-            $table->enum('status_pengantaran', ['Menunggu', 'Terkirim'])->default('Menunggu');
+            $table->enum('status_pengantaran', ['Menunggu', 'Sedang Dikirim', 'Terkirim'])->default('Menunggu');
             $table->timestamp('delivered_at')->nullable();
             $table->timestamps();
         });
