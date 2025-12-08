92efcdfad23d5b490702b09552e8d13fe4ea6278
bokerface <waskitodamar17@gmail.com>
2025-12-08 16:09:50 +0700

    nambahi kolom status jadwal produksi ng table list

--- app/Filament/Resources/ProductionSchedules/Tables/ProductionSchedulesTable.php
+++ app/Filament/Resources/ProductionSchedules/Tables/ProductionSchedulesTable.php
@@ -26,15 +26,34 @@ class ProductionSchedulesTable
                             ->translatedFormat('l, d F Y')
                     )
                     ->sortable(),
-                TextColumn::make('jumlah_porsi_besar')
-                    ->label('porsi besar')
-                    ->getStateUsing(function (ProductionSchedule $record): int|float {
-                        return $record->total_porsi_besar;
-                    }),
-                TextColumn::make('jumlah_porsi_kecil')
-                    ->label('porsi kecil')
-                    ->getStateUsing(function (ProductionSchedule $record): int|float {
-                        return $record->total_porsi_kecil;
+                // TextColumn::make('jumlah_porsi_besar')
+                //     ->label('porsi besar')
+                //     ->getStateUsing(function (ProductionSchedule $record): int|float {
+                //         return $record->total_porsi_besar;
+                //     }),
+                // TextColumn::make('jumlah_porsi_kecil')
+                //     ->label('porsi kecil')
+                //     ->getStateUsing(function (ProductionSchedule $record): int|float {
+                //         return $record->total_porsi_kecil;
+                //     }),
+                TextColumn::make('status')
+                    ->label('Status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'Direncanakan' => 'warning',
+                        'Menunggu ACC Kepala SPPG' => 'info',
+                        'Terverifikasi' => 'success',
+                        'Didistribusikan' => 'warning',
+                        'Selesai' => 'success',
+                        default => 'gray',
+                    })
+                    ->icon(fn(string $state): string => match ($state) {
+                        'Direncanakan' => 'heroicon-m-clipboard-document-list',
+                        'Menunggu ACC Kepala SPPG' => 'heroicon-m-clock',
+                        'Terverifikasi' => 'heroicon-m-document-check',
+                        'Didistribusikan' => 'heroicon-m-truck',
+                        'Selesai' => 'heroicon-m-check-circle',
+                        default => null,
                     }),
                 TextColumn::make('sppg.nama_sppg')->label('SPPG'),
                 TextColumn::make('menu_hari_ini')->label('Menu')->sortable()->searchable(),
