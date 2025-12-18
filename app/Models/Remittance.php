1d20715752ae3398c9695d95f0edca7a981d78e9
bokerface <waskitodamar17@gmail.com>
2025-12-18 20:39:31 +0700

    ngaktifke soft delete

--- app/Models/Remittance.php
+++ app/Models/Remittance.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Remittance extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'bill_id',
         'user_id',
