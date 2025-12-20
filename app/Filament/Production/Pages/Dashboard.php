fe9beec0889c226832898d460175c2ccdfa30754
bokerface <waskitodamar17@gmail.com>
2025-12-20 21:58:48 +0700

    mbenakne mobile layout panel production

---
+++ app/Filament/Production/Pages/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Production\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static string $layout = 'layouts.mobile-navigation';
+}
--- app/Providers/Filament/ProductionPanelProvider.php
+++ app/Providers/Filament/ProductionPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Production\Pages\Dashboard;
 use App\Filament\Production\Pages\Delivery;
 use App\Filament\Production\Pages\Distribution;
 use App\Filament\Production\Pages\Verify;
@@ -11,7 +12,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages\Dashboard;
+// use Filament\Production\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
