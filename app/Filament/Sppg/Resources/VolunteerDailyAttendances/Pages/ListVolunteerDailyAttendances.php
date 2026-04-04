980b3db42fc18b269d9541f80bc5d21118a4d58a
yaufani adam <yaufaniadam@gmail.com>
2026-04-04 10:16:16 +0700

    Feat: add Presensi action button to Volunteer Daily Attendance list

--- app/Filament/Sppg/Resources/VolunteerDailyAttendances/Pages/ListVolunteerDailyAttendances.php
+++ app/Filament/Sppg/Resources/VolunteerDailyAttendances/Pages/ListVolunteerDailyAttendances.php
@@ -13,6 +13,11 @@ class ListVolunteerDailyAttendances extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            \Filament\Actions\Action::make('presensi')
+                ->label('Presensi')
+                ->icon('heroicon-o-clipboard-document-check')
+                ->color('primary')
+                ->url(fn (): string => \App\Filament\Sppg\Pages\DailyAttendance::getUrl()),
             CreateAction::make(),
         ];
     }
